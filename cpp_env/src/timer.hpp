4ea7c93bcf99b1eb0a513c834855fcb4ff66961b
afterburner <rockey1024@icloud.com>
2019-01-21 23:11:03 +0800

    Remove tasks.hpp

--- cpp_env/src/timer.hpp
+++ cpp_env/src/timer.hpp
@@ -10,6 +10,9 @@
 #include <map>     // for map
 #include <string>  // for string
 #include <utility> // for move
+#include <iostream>
+#include <iomanip>
+
 
 enum class Metric
 {
@@ -27,10 +30,33 @@ struct Timer
     Timer(Timer &&other) = default;
     Timer &operator=(const Timer &other) = delete;
     Timer &operator=(Timer &&other) = default;
-    virtual ~Timer();
+     ~Timer()
+    {
+        in_file.open(file_name);
+        long old_time;
+        std::cout << std::setw(27)<<" "<< "current    "<<"previous " << '\n';
+        for (const auto &[session, new_time] : _time_by_session)
+        {
+            in_file >> old_time;
+            std::cout << std::setw(29) << std::setiosflags(std::ios::left) << session << std::setw(5) << new_time << "\t\t" << std::setw(6) << old_time << "       " << (metric == Metric::second ? "seconds" : "ms") << '\n';
+        }
+        in_file.close();
+        out_file.open(file_name, std::ofstream::trunc | std::ofstream::out);
+        for (const auto &pair : _time_by_session)
+        {
+            out_file << pair.second << ' ';
+        }
+        out_file.close();
+    }
 
-    void stop(std::string session_name);
-    void start();
+    void stop(const std::string &session_name){
+        _tp_current = Clock::now();
+        long time_elapsed = metric == Metric::second ? std::chrono::duration_cast<std::chrono::seconds>(_tp_current - _tp_start).count() : std::chrono::duration_cast<std::chrono::milliseconds>(_tp_current - _tp_start).count();
+        _time_by_session["[" + session_name + "]"] = time_elapsed;
+        _tp_start = _tp_current;
+    }
+    void start(){ _tp_start = Clock::now(); }
+
 
 private:
     timePoint _tp_start, _tp_current;
--- templates/cppinit.sh
+++ templates/cppinit.sh
@@ -13,6 +13,7 @@ mkdir $PORJECT_NAME
 prefix=@INSTALL_DIR@
 CONFIG_FILE_DIR="$prefix"/cpp_env
 cp -r $CONFIG_FILE_DIR/. $PORJECT_NAME
+git init $(pwd)/$PORJECT_NAME
 echo 'Created c++ project folder:'
 echo \<$(pwd)/$PORJECT_NAME\> is sent to the clipboard
 echo $(pwd)/$PORJECT_NAME | xclip -sel clip
